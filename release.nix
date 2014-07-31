f10a71f5e7edaa259691070633763d8603a0bebb
Sönke Hahn <soenkehahn@gmail.com>
2014-07-31 15:49:13 +0800

    install.sh: filter pipes, .git, result, dist and .nhc

--- release.nix
+++ release.nix
@@ -1,6 +1,11 @@
 {
   pkgs ? import <nixpkgs> {},
-  src ? ./.
+  src ? builtins.filterSource (path: type:
+    type != "unknown" &&
+    baseNameOf path != ".git" &&
+    baseNameOf path != "result" &&
+    baseNameOf path != "dist" &&
+    baseNameOf path != ".nhc") ./.
 } : pkgs.haskellPackages.buildLocalCabalWithArgs {
   inherit src;
   name = "nhc";
